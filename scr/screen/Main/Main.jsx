f84e5d0795627baf01e3bf1a16203b612c92a1d5
ateequlrehman94 <ateequlrehman94@gmail.com>
2022-12-22 12:31:57 +0500

    Showstudents and refresh

--- scr/screen/Main/Main.jsx
+++ scr/screen/Main/Main.jsx
@@ -1,26 +1,76 @@
-import { useState } from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  FlatList,
+} from "react-native";
 import Toast from "react-native-toast-message";
 import { FloatingAction } from "react-native-floating-action";
 import { Loading } from "../../components/loading";
 import Swiper from "react-native-swiper";
 import { Addstudent } from "../../components/addstudent";
+import { firebase } from "../../services/firebaseConfig";
 
 function Main() {
   const [showAddstudent, setShowAddstudent] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
+  const [studentData, setstudentData] = useState([]);
+  useEffect(() => {
+    fetchstudentFromDB();
+  }, []);
+  const fetchstudentFromDB = () => {
+    setShowLoading(true);
+    firebase
+      .firestore()
+      .collection("Student")
+      .get()
+      .then((response) => {
+        setstudentData(response.docs);
+        setShowLoading(false);
+      })
+      .catch((error) => {
+        console.log({ error });
+        setShowLoading(false);
+      });
+  };
+  const __renderItem = ({ item }) => {
+    const student = item.data();
+    return (
+      <View>
+        <ImageBackground
+          style={{ width: 100, height: 100, margin: 5 }}
+          source={{ uri: student.StudentImageUrl }}
+        ></ImageBackground>
+        <Text>{student.studentName}</Text>
+        <Text>{student.fatherName}</Text>
+        <Text>{student.Address}</Text>
+      </View>
+    );
+  };
   return (
     <View style={{ flex: 1 }}>
+      <FlatList
+        data={studentData}
+        horizontal={true}
+        renderItem={__renderItem}
+        ListEmptyComponent={<Text>no recipies found </Text>}
+        refreshing={showLoading}
+        onRefresh={() => fetchstudentFromDB()}
+      />
+      <Toast />
       <FloatingAction
         color={"red"}
         onPressMain={() => {
           setShowAddstudent(true);
         }}
       />
-
       <Addstudent
         show={showAddstudent}
         onClose={() => setShowAddstudent(false)}
       />
+      {showLoading && <Loading />}
     </View>
   );
 }
